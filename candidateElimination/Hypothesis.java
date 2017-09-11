7883769489befee4b0597e386016e648fb0b1ad2
vamshiaruru <vamshiaruru0@gmail.com>
2017-09-11 09:54:50 +0530

    Made some implementational changes to class Hypothesis

--- candidateElimination/Hypothesis.java
+++ candidateElimination/Hypothesis.java
@@ -1,65 +1,65 @@
 package candidate;
 
 import java.util.*;
-// package candidate;
 
 class Hypothesis{
+    final int ALL = 999;
+    final int NONE = 111;
+    final int HAIR = 0;
+    final int FEATHERS = 1;
+    final int EGGS = 2;
+    final int MILK = 3;
+    final int AIRBORNE = 4;
+    final int AQUATIC = 5;
+    final int PREDATOR = 6;
+    final int TOOTHED = 7;
+    final int BACKBONE = 8;
+    final int BREATHES = 9;
+    final int VENOMOUS = 10;
+    final int FINS = 11;
+    final int TAIL = 12;
+    final int DOMESTIC = 13;
+    final int CATSIZE = 14;
+    final int LEGS = 15;
+    final int TYPE = 16;
     String name;
     int hair, feathers, eggs, milk, airborne, aquatic, predator, toothed;
     int backbone, breathes, venomous, fins, tail, domestic, catsize;
     int legs, type;
+    int[] attributes;
+    Hypothesis(int[] attributes){
+        this.attributes = attributes;
+    }
     public void setName(String name){
         this.name = name;
     }
-    public void setHair(int hair){
-        this.hair = hair;
+    public boolean isEqual(int attr1, int attr2){
+        if((attr1 == NONE && attr2 != NONE)||(attr2 == NONE && attr1 !=NONE)){
+            return false;
+        }
+        else if(attr1 == ALL || attr2 == ALL){
+            return true;
+        }
+        else if(attr1 == attr2){
+            return true;
+        }
+        else {
+            return false;
+        }
+    } 
+    public boolean isConsistent(Hypothesis h){ // h is the training data
+        int count = 0;
+        for(int i=0;i<h.attributes.length;i++){
+            if(isEqual(this.attributes[i], h.attributes[i])){
+                count++;
+            }
+        }
+        if(count == h.attributes.length){
+            return true;
+        }
+        return false;
     }
-    public void setFeathers(int feathers){
-        this.feathers = feathers;
-    }
-    public void setEggs(int eggs){
-        this.eggs = eggs;
-    }
-    public void setMilk(int milk){
-        this.milk = milk;
-    }
-    public void setAirborne(int airborne){
-        this.airborne = airborne;
-    }
-    public void setAquatic(int aquatic){
-        this.aquatic = aquatic;
-    }
-    public void setPredator(int predator){
-        this.predator = predator;
-    }
-    public void setToothed(int toothed){
-        this.toothed = toothed;
-    }
-    public void setBackbone(int backbone){
-        this.backbone = backbone;
-    }
-    public void setBreathes(int breathes){
-        this.breathes = breathes;
-    }
-    public void setVenomous(int venomous){
-        this.venomous = venomous;
-    }
-    public void setFins(int fins){
-        this.fins = fins;
-    }
-    public void setLegs(int legs){
-        this.legs = legs;
-    }
-    public void setTail(int tail){
-        this.tail = tail;
-    }
-    public void setDomestic(int domestic){
-        this.domestic = domestic;
-    }
-    public void setCatsize(int catsize){
-        this.catsize = catsize;
-    }
-    public void setType(int type){
-        this.type = type;
+    public void isMoreGeneral(Hypothesis h){
+
     }
 }
--- candidateElimination/Reader.java
+++ candidateElimination/Reader.java
@@ -12,25 +12,13 @@ class Reader{
         Scanner in = new Scanner(new FileReader(filename));
         while(in.hasNext()){
             String[] columns = in.next().split(",");
-            Hypothesis hypo = new Hypothesis();
+            int[] attributes = {0};
+            int j = 0;
+            for (int i=1;i<columns.length;i++ ) {
+                attributes[j++] = Integer.parseInt(columns[i]);
+            }
+            Hypothesis hypo = new Hypothesis(attributes);            
             hypo.setName(columns[0]);
-            hypo.setHair(Integer.parseInt(columns[1]));
-            hypo.setFeathers(Integer.parseInt(columns[2]));
-            hypo.setEggs(Integer.parseInt(columns[3]));
-            hypo.setMilk(Integer.parseInt(columns[4]));
-            hypo.setAirborne(Integer.parseInt(columns[5]));
-            hypo.setAquatic(Integer.parseInt(columns[6]));
-            hypo.setPredator(Integer.parseInt(columns[7]));
-            hypo.setToothed(Integer.parseInt(columns[8]));
-            hypo.setBackbone(Integer.parseInt(columns[9]));
-            hypo.setBreathes(Integer.parseInt(columns[10]));
-            hypo.setVenomous(Integer.parseInt(columns[11]));
-            hypo.setFins(Integer.parseInt(columns[12]));
-            hypo.setLegs(Integer.parseInt(columns[13]));
-            hypo.setTail(Integer.parseInt(columns[14]));
-            hypo.setDomestic(Integer.parseInt(columns[15]));
-            hypo.setCatsize(Integer.parseInt(columns[16]));
-            hypo.setType(Integer.parseInt(columns[17]));
             hypothesis.add(hypo);
         }
         return hypothesis;
