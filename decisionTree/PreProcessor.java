36b8d9dbd6571dc504d1fc5e1a86b0730956951b
vamshiaruru <vamshiaruru0@gmail.com>
2017-09-25 00:18:57 +0530

    compiled successfully

--- decisionTree/PreProcessor.java
+++ decisionTree/PreProcessor.java
@@ -2,11 +2,17 @@ package decision_tree;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.*;
 
 class PreProcessor{
     TrainingData[] examples;
+    int[] continuousValues = {0, 2, 4, 10, 11, 12};
+    double[] thresholds = new double[6]; 
+    double Entropy;
+
     PreProcessor(TrainingData[] examples){
         this.examples = examples;
+        getEntropy();
     }
     void sortExamples(int i){
         Arrays.sort(examples, new Comparator<TrainingData>(){
@@ -15,5 +21,108 @@ class PreProcessor{
                 return t1.attributes[i].compareTo(t2.attributes[i]);
             }
         });
+    }
+    void makeDiscrete(){
+        for(int i = 0; i < this.continuousValues.length; i++){
+            int index = this.continuousValues[i];
+            sortExamples(index);
+            String thresh = this.examples[0].attributes[index];
+            double threshold = Double.parseDouble(thresh);
+            double informationGain = getInformationGain(index, threshold);
+            for(int j = 0; j < this.examples.length - 1; j++){
+                boolean curr = this.examples[j].getValue();
+                boolean next = this.examples[j+1].getValue();
+                if(curr != next){
+                    double current = Double.parseDouble(examples[j].attributes[index]);
+                    double after = Double.parseDouble(examples[j+1].attributes[index]);
+                    double currIG = getInformationGain(index,(current+after)/2);
+                    if(currIG > informationGain){
+                        informationGain = currIG;
+                        threshold = (current + after)/2;
+                    }
+                }
+            }
+            this.thresholds[i] = threshold;
+        }
+    }
+    void getEntropy(){
+        int positiveExamples = 0;
+        int negativeExamples = 0;
+        for (TrainingData example : this.examples) {
+            if(example.getValue()){
+                positiveExamples++;
+            }
+            negativeExamples++;
+        }
+        double posWeight = positiveExamples/this.examples.length;
+        double negWeight = negativeExamples/this.examples.length;
+        double entropy = -posWeight*(Math.log(posWeight)/Math.log(2)) 
+                         - negWeight*(Math.log(negWeight)/Math.log(2));
+        this.Entropy = entropy;                         
+    }
+    double getInformationGain(int index){
+        double currEntropy = 0;
+        String[] values = TrainingData.getAcceptedValues(index);
+        for(String value: values){
+            int posExamples = 0;
+            int negExamples = 0;
+            int len = 0;
+            for (TrainingData example : this.examples) {
+                if(example.attributes[index].equalsIgnoreCase(value)){
+                    len++;
+                    if(example.getValue()){
+                        posExamples++;  
+                    }else{
+                        negExamples++;
+                    }
+                }
+            }
+            double weight = -1 * len/this.examples.length;
+            double posWeight = posExamples/len;
+            double negWeight = negExamples/len;
+            double entropy = posWeight*(Math.log(posWeight)/Math.log(2)) 
+                             + negWeight*(Math.log(negWeight)/Math.log(2));
+            currEntropy += (weight*entropy);
+        }
+        return this.Entropy - currEntropy;
+    }
+    double getInformationGain(int index, double threshold){
+        double currEntropy = 0;
+        int posExamplesAboveThreshold = 0;
+        int negExamplesAboveThreshold = 0;
+        int posExamplesBelowThreshold = 0;
+        int negExamplesBelowThreshold = 0;
+        int lenAboveThreshold = 0;
+        int lenBelowThreshold = 0;
+        for (TrainingData example : this.examples) {
+            if (Integer.parseInt(example.attributes[index]) > threshold){
+                lenAboveThreshold++;
+                if(example.getValue()){
+                    posExamplesAboveThreshold++;
+                }else{
+                    negExamplesAboveThreshold++;
+                }
+            }else{
+                lenBelowThreshold++;
+                if(example.getValue()){
+                    posExamplesBelowThreshold++;
+                }else{
+                    negExamplesBelowThreshold++;
+                }
+            }
+        }
+        double aboveThresholdWeight = -1 * lenAboveThreshold/examples.length;
+        double posWeightAbove = posExamplesAboveThreshold/lenAboveThreshold;
+        double negWeightAbove = negExamplesAboveThreshold/lenAboveThreshold;
+        double entropyAbove = posWeightAbove * (Math.log(posWeightAbove)/Math.log(2)) 
+                         + negWeightAbove * (Math.log(negWeightAbove)/Math.log(2));
+        double belowThresholdWeight = -1 * lenBelowThreshold/examples.length;
+        double posWeightBelow = posExamplesBelowThreshold/lenBelowThreshold;
+        double negWeightBelow = negExamplesBelowThreshold/lenBelowThreshold;
+        double entropyBelow = posWeightBelow * (Math.log(posWeightBelow)/Math.log(2)) 
+                         + negWeightBelow * (Math.log(negWeightBelow)/Math.log(2));
+        currEntropy = aboveThresholdWeight * entropyAbove
+                     + belowThresholdWeight * entropyBelow;
+        return this.Entropy - currEntropy;
     }
 }
--- decisionTree/TrainingData.java
+++ decisionTree/TrainingData.java
@@ -12,4 +12,64 @@ class TrainingData implements Comparable<TrainingData>{
     public int compareTo(TrainingData t){
         return 0;
     }
+    static String[] getAcceptedValues(int i){
+        String[] accepted_values;
+        if (i == 1){
+            accepted_values = new String[]{"Private", "Self-emp-not-inc",
+                             "Self-emp-inc", "Federal-gov", "Local-gov", 
+                             "State-gov","Without-pay", "Never-worked"};
+        }
+        else if (i == 3){
+            accepted_values = new String[]{"Bachelors", "Some-college", "11th", 
+             "Prof-school", "Assoc-acdm", "Assoc-voc", "9th", "7th-8th", "12th",
+             "Masters", "1st-4th", "10th", "Doctorate", "5th-6th",
+             "Preschool", "HS-grad",};
+        }
+        else if (i == 5){
+            accepted_values = new String[]{"Married-civ-spouse", "Divorced",
+            "Separated", "Widowed","Married-spouse-absent", "Married-AF-spouse"
+            ,"Never-married"};
+        }
+        else if (i == 6){
+            accepted_values = new String[]{"Tech-support", "Craft-repair",
+             "Sales", "Exec-managerial", "Prof-specialty", "Handlers-cleaners",
+             "Machine-op-inspct", "Adm-clerical", "Farming-fishing",
+             "Transport-moving", "Priv-house-serv", "Protective-serv",
+             "Armed-Forces", "Other-service"};
+        }
+        else if (i == 7){
+            accepted_values = new String[]{"Wife", "Own-child", "Husband",
+            "Other-relative", "Unmarried", "Not-in-family"};
+        }
+        else if (i == 8){
+            accepted_values = new String[]{"White", "Asian-Pac-Islander",
+             "Amer-Indian-Eskimo", "Other", "Black"};
+        }
+        else if (i == 9){
+            accepted_values = new String[]{"Female", "Male"};
+        }
+        else if (i == 13){
+            accepted_values = new String[]{"United-States", "Cambodia", "England",
+             "Puerto-Rico", "Canada", "Germany", "Outlying-US(Guam-USVI-etc)",
+             "India", "Japan", "Greece", "South", "China", "Cuba", "Iran",
+             "Honduras", "Philippines", "Italy", "Poland", "Jamaica",
+             "Vietnam", "Mexico","Portugal","Ireland", "France",
+             "Dominican-Republic", "Laos", "Ecuador", "Taiwan", "Haiti",
+             "Columbia", "Hungary", "Guatemala", "Nicaragua", "Scotland",
+             "Thailand", "Yugoslavia", "El-Salvador", "Trinadad&Tobago", "Peru",
+             "Hong", "Holand-Netherlands"};
+        }
+        else{
+            accepted_values = null;
+        }
+        return accepted_values;
+
+    }
+    boolean getValue(){
+        // <=50k => negativeValue.
+        if(this.attributes[attributes.length - 1].equalsIgnoreCase("<=50K")){
+            return false;
+        }
+        return true;
+    }
 }
