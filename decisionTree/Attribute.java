1ed9a8f998173f6c3e6826fd4410ea39167735a0
vamshiaruru <vamshiaruru0@gmail.com>
2017-09-25 19:19:22 +0530

    attribute class added

--- decisionTree/Attribute.java
+++ decisionTree/Attribute.java
@@ -1,7 +1,29 @@
 package decision_tree;
 
-imort java.util.*;
+import java.util.*;
 
 class Attribute{
-
+    String name;
+    String[] acceptableValues;
+    int index;
+    Attribute(int i){
+        this.index = i;
+        this.name = TrainingData.features.get(i);
+        this.acceptableValues = TrainingData.getAcceptedValues(i);
+    }
+    boolean equalTo(Attribute a){
+        if(a.name.equalsIgnoreCase(this.name) && 
+            a.acceptableValues.equals(this.acceptableValues)){
+            return true;
+        }
+        return false;
+    }
+    boolean isAcceptableValue(String value){
+        for (String val: this.acceptableValues ) {
+            if(val.equalsIgnoreCase(value)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
